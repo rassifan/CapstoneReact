640393afea68585701ef7c845579c12e219022c6
Sifan Tilahun <sifantilahun54@gmail.com>
2022-12-20 16:50:49 +0300

    Fetching the images from the API

--- capstone/caps/caps/src/context/PhotoContext.js
+++ capstone/caps/caps/src/context/PhotoContext.js
@@ -1,11 +1,18 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 
 const Context = React.createContext()
 
 function ContextProvider({children}) {
-    const [array, setArray] = React.useState([])
+    const [allPhotos, setAllPhotos] = useState([])
+    useEffect(() => {
+    fetch("https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json")
+    .then(response => response.json())
+        // 4. Setting *dogImage* to the image url that we received from the response above
+    .then(data => setAllPhotos(data))
+    },[])
+    
     return (
-        <Context.Provider value={{array}}>
+        <Context.Provider value={{allPhotos}}>
             {children}
         </Context.Provider>
     )
