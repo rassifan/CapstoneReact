66c374d7bf14a996e37e2b898bc448e0fc2d95b6
Sifan Tilahun <sifantilahun54@gmail.com>
2022-12-21 14:02:00 +0300

    Update Header.js

--- capstone/caps/caps/src/components/Header.js
+++ capstone/caps/caps/src/components/Header.js
@@ -1,11 +1,15 @@
-import React from "react"
+import {React, useContext} from "react"
 import {Link} from "react-router-dom"
+import {Context} from "../context/PhotoContext"
 
 function Header() {
+    const {toggleFavorite, addToCart, cartItems, removeFromCart} = useContext(Context)
     return (
         <header>
             <Link to="/"><h2>Pic Some</h2></Link>
-            <Link to="/cart"><i className="ri-shopping-cart-line ri-fw ri-2x"></i></Link>
+            <Link to="/cart">
+                <i className={`ri-shopping-cart-${Object.entries(cartItems).length === 0 ? "line" : "fill"} ri-fw ri-2x`}></i>
+            </Link>
         </header>
     )
 }
